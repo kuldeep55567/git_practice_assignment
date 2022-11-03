5c680a67520316ae8690f3d47991d33c96e5257f
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2022-11-03 22:21:41 +0530

    code is fixed

--- prime.js
+++ prime.js
@@ -1,8 +1,17 @@
 function checkprime(num){
+  let count = 0;
     for(let i=1;i<=num;i++){
-        if(num%1){
-            return true;
+        if(num %i == 0){
+            count++
         }
     }
+  
+  if(count == 2){
+    return true;
+  }else{
+    return false;
+  }
 }
-checkprime();
+
+let x = checkprime(13);
+console.log(x);
